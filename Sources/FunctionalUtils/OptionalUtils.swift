362d7d972cb95020d04f15a27b91ccd0970b7d22
Franco Consoni <franco@goprebo.com>
2020-12-26 00:50:14 -0300

    Depured tools

--- Sources/FunctionalUtils/OptionalUtils.swift
+++ Sources/FunctionalUtils/OptionalUtils.swift
@@ -9,21 +9,6 @@ import Foundation
 
 //SCALA based extension
 extension Optional {
-	public func get(_ error: Error? = nil) throws -> Wrapped {
-		switch self {
-		case .none:
-			throw error ?? NSError(domain: "No wrapped value: \(String(describing: self))", code: 0, userInfo: nil)
-		case .some(let some): return some
-		}
-	}
-	
-	public func filter(_ fx: (Wrapped) -> Bool) -> Optional {
-		switch self {
-		case .none: return self
-		case .some(let some): return fx(some) ? self : .none
-		}
-	}
-	
 	public func isEmpty() -> Bool {
 		switch self {
 		case .some(_): return false
@@ -48,16 +33,4 @@ extension Optional {
 	public func onValue(_ fx: (Wrapped) -> Void) {
 		self.fold((), fx)
 	}
-}
-
-public func getOrElse<A>(_ defValue: A) -> (A?) -> A {
-	return { optional in optional.getOrElse(defValue) }
-}
-
-public func isEmpty<A>(_ optional: A?) -> Bool {
-	return optional.isEmpty()
-}
-
-public func fold<T, U>(_ ifEmpty: U, _ tx: @escaping (T) -> U) -> (T?) -> U {
-	return { optional in optional.fold(ifEmpty, tx) }
 }
